2d768ea0c186b42d1cbefbac85183ee501fa37e4
Juan Bonino <tomasboni9@gmail.com>
2022-09-17 14:41:13 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { LessonModule } from './lesson/lesson.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+    }),
+    LessonModule,
+  ],
+})
+export class AppModule { }
