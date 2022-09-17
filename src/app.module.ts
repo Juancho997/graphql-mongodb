ebd345dbbdf723b611f6439907d3cd600b13d64c
Juan Bonino <tomasboni9@gmail.com>
2022-09-17 18:06:58 -0300

    feature: mongodb connection and lesson entity sync

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
 import { LessonModule } from './lesson/lesson.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb+srv://juanb:2mwcyonZ19TBbLs1@cluster0.hcxeeaa.mongodb.net/test',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson]
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql',
